96e9c45a2eb5867bc21feb75d2b4bd46c8dcdbf5
Sebastian Ojefors <godnoken@gmail.com>
2022-08-30 15:52:18 +0200

    Add user input and coordinates api call

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-async function getWeatherData() {
-    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=61d324f1999587686e64cc75ed85aad0");
-    const data = await response.json();
+const locationInput = document.querySelector("input");
 
-    console.log(data)
+locationInput.addEventListener("change", changeLocation);
+
+
+async function getCoordinatesData(location) {
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=61d324f1999587686e64cc75ed85aad0`);
+    const data = await response.json();
 
     return data;
 }
 
-getWeatherData();
+
+async function getWeatherData(lat, lon) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=61d324f1999587686e64cc75ed85aad0`);
+    const data = await response.json();
+
+    return data;
+}
+
+
+async function changeLocation(event) {
+    const coordinatesData = await getCoordinatesData(event.target.value);
+    const weatherData = await getWeatherData(coordinatesData[0].lat, coordinatesData[0].lon);
+
+    console.log(weatherData);
+}
