24e37c38a98712f734cd2fbad2e5c246a28e2cdd
Sebastian Ojefors <godnoken@gmail.com>
2022-09-04 14:17:30 +0200

    Add opacity animation to sun

--- createSun.js
+++ createSun.js
@@ -1,6 +1,12 @@
-const sunElement = document.querySelector(".sun");
+//const sunElement = document.querySelector(".sun");
+const animatedBackgroundElement = document.querySelector(".animated-background")
 
 export function moveSun(timeData) {
+  const sunElement = document.createElement("div");
+  sunElement.classList.add("sun");
+
+  
+
   const sunTimeline = gsap.timeline();
 
   const sunriseTimeInDecimals =
@@ -19,47 +25,24 @@ export function moveSun(timeData) {
   const timeUntilSunsetInSeconds = (sunsetTimeInDecimals - currentTimeInDecimals) * 3600;
   const currentSunPosition = hoursSinceSunrise / hoursOfSunshine;
 
-  console.log(
-    sunriseTimeInDecimals,
-    sunsetTimeInDecimals,
-    currentTimeInDecimals,
-    currentSunPosition
-  );
-
   const windowWidth = window.innerWidth;
   const windowHeight = window.innerHeight;
 
   sunTimeline.set(sunElement, {
     x: 0 - sunElement.offsetWidth / 2,
     y: windowHeight + sunElement.offsetHeight,
+    opacity: 0
   });
+  
 
-  createRays();
 
-  const rays = document.querySelectorAll(".ray");
-  function isEven(n) {
-    return n % 2 == 0;
-  }
 
-  for (var i = 0; i < rays.length; i++) {
-    const thisRand = Math.floor(Math.random() * 100) + 10;
-    let thisRotation = 360;
-
-    if (isEven(i)) {
-      thisRotation = -360;
-    }
-
-    const tl = gsap.timeline({ repeat: -1 });
-    tl.from(rays[i], 0, {
-      rotation: 0,
-      scale: 1,
-    }).to(rays[i], thisRand, {
-      rotation: thisRotation,
-      scale: 2,
-    });
-  }
 
   const path = [
+    {
+      x: 0 - sunElement.offsetWidth / 2,
+      y: windowHeight + sunElement.offsetHeight
+    },
     { x: windowWidth - windowWidth / 1.25, y: windowHeight / 3 },
     { x: windowWidth / 2 - sunElement.offsetWidth, y: windowHeight / 10 },
     { x: windowWidth / 1.25 - sunElement.offsetWidth, y: windowHeight / 3 },
@@ -85,15 +68,50 @@ export function moveSun(timeData) {
     motionPath: motionPath,
     duration: timeUntilSunsetInSeconds,
     ease: "none",
-  });
-
+  }, 0);
+  
+  
   sunTimeline.progress(currentSunPosition);
-  setInterval(() => {
-    //console.log(sunTimeline.progress())
-  }, 10);
+
+  // Separate timeline due to progress being set above
+  // in turn cancelling the opacity animation
+  gsap.to(sunElement, {
+    duration: 4,
+    opacity: 1
+  })
+
+
+  animatedBackgroundElement.appendChild(sunElement);
+
+  createRays();
+
+  const rays = document.querySelectorAll(".ray");
+  function isEven(n) {
+    return n % 2 == 0;
+  }
+
+  for (var i = 0; i < rays.length; i++) {
+    const thisRand = Math.floor(Math.random() * 100) + 10;
+    let thisRotation = 360;
+
+    if (isEven(i)) {
+      thisRotation = -360;
+    }
+
+    const tl = gsap.timeline({ repeat: -1 });
+    tl.from(rays[i], 0, {
+      rotation: 0,
+      scale: 1,
+    }).to(rays[i], thisRand, {
+      rotation: thisRotation,
+      scale: 2,
+    });
+  }
 }
 
 function createRays() {
+  const sunElement = document.querySelector(".sun");
+
   const uvIndex = 2;
 
   for (let i = 0; i < uvIndex * 5; i++) {
